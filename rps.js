c69bc824011db023e13e4620d69969c25bcae387
vmkiss <veronika.kiss17@gmail.com>
2024-06-05 13:31:22 -0700

    write and test getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+//Get computer's move
+
+function getComputerChoice(){
+    computerChoice = Math.random()
+    
+    if (computerChoice < (1/3)) {
+       computerChoice = "Rock"; 
+    } else if (computerChoice > (2/3)) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice
+}
+
+for (i = 0; i < 11; ++i){
+    console.log(getComputerChoice())
+}
+    
